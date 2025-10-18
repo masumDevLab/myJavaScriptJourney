08f8d4b3eb61693741b4f0b5e04a4c677058ec48
masumDevLab <masumkabir847@gmail.com>
2025-10-18 23:20:13 +0800

    Add files via upload

---
+++ chapTwo/number.js
@@ -0,0 +1,10 @@
+var n = 34;
+var m = 99.34;
+var nn = Number('34.34');
+console.log(nn);
+console.log(m);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(1/0);
+console.log("abc" * 10);
+console.log(m+n);
